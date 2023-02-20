4932cdb8aa5f4e8bd480ee4cda0b5e8bdeb751ad
김민경 <mkkim@4dreplay.com>
2023-02-20 13:38:39 +0900

    :sparkles: feat
    line shape controller move as other shapes

--- src/shapes/shape.factory.ts
+++ src/shapes/shape.factory.ts
@@ -3,9 +3,13 @@ import { JoystickShape } from "../enums/shape.enum";
 export const shapeFactory = (shape: JoystickShape, size: number) => {
   switch (shape) {
     case JoystickShape.Circle:
-      return { height: `${size}px`, width: `${size}px`, borderRadius: size };
+      return { height: size, width: size, borderRadius: "50%" };
     case JoystickShape.Square:
-      return { height: `${size}px`, width: `${size}px`, borderRadius: Math.sqrt(size) };
+      return {
+        height: size,
+        width: size,
+        borderRadius: Math.sqrt(size),
+      };
     case JoystickShape.Line:
       return {
         width: `${size}rem`,
